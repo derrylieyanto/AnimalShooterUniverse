d9e0b4d400f9a9c3d9993335543b4d8a737c9e09
derrylieyanto <32271541+derrylieyanto@users.noreply.github.com>
2018-09-30 13:37:18 +0700

    Add Project

---
+++ AnimalShooterUniverse/AnimalShooterUniverse/TitleForm.vb
@@ -0,0 +1,9 @@
+﻿Public Class AnimalShooterUniverse
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim CharSelectForm As New CharSelectForm
+
+        Me.Hide()
+        CharSelectForm.Show()
+
+    End Sub
+End Class
