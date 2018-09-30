db963fbedb5e5d1b7c002105a9aa5408d5b4ff56
derrylieyanto <32271541+derrylieyanto@users.noreply.github.com>
2018-09-30 14:32:39 +0700

    add Class Animal

---
+++ AnimalShooterUniverse/AnimalShooterUniverse/Animal.vb
@@ -0,0 +1,30 @@
+﻿Public Class Animal
+    Private mstrhp As Integer
+    Private mstrdamage As Integer
+
+
+    Public Sub New(newhp As Integer, newdamage As Integer)
+        mstrhp = newhp
+        mstrdamage = newdamage
+    End Sub
+
+
+    Public Property Life() As Integer
+        Get
+            Return mstrhp
+        End Get
+        Set(ByVal value As Integer)
+            mstrhp = value
+        End Set
+    End Property
+
+    Public Property Damage() As Integer
+        Get
+            Return mstrdamage
+        End Get
+        Set(ByVal value As Integer)
+            mstrdamage = value
+        End Set
+    End Property
+
+End Class
--- AnimalShooterUniverse/AnimalShooterUniverse/CharSelectForm.vb
+++ AnimalShooterUniverse/AnimalShooterUniverse/CharSelectForm.vb
@@ -1,3 +1,10 @@
 ﻿Public Class CharSelectForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim mainForm As New MainForm
 
+        Me.Hide()
+        mainForm.Show()
+
+
+    End Sub
 End Class
